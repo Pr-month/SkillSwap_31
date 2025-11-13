f9c79984b6de7361cd45bb6c0397b36158e8ec61
stan99 <stan99-cheb@yandex.ru>
2025-11-13 11:45:12 +0300

    feat: поключение JwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'superSecretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
