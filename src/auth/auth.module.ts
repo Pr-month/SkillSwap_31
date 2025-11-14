7548f1b5975de8e82bd64149b6292ab0280ce1ba
stan99 <stan99-cheb@yandex.ru>
2025-11-14 13:46:46 +0300

    feat: создание сервиса Auth. Объединяет в себе задачи по созданию роутов, гард и стратегий

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule, JwtModuleOptions, JwtSignOptions } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): JwtModuleOptions => {
-        const secret = configService.get<string>('JWT_SECRET', 'superSecretKey');
-        const rawExpires = configService.get<string>('JWT_EXPIRES_IN');
-        const expiresIn = (rawExpires ?? '1h') as JwtSignOptions['expiresIn'];
-        const signOptions: JwtSignOptions = {
-          expiresIn,
-        };
-        return {
-          secret,
-          signOptions,
-        };
-      },
+    JwtModule.register({
+      secret: 'default_secret',
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
---
+++ src/auth/strategies/jwt-access.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtAccessStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-access',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'access_secret',
+    });
+  }
+
+  validate(payload: { name: string; email: string }) {
+    return payload;
+  }
+}
