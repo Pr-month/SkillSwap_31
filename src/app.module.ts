ad2240acd08a448500885eee03f3563bf2494fa1
Elelna Shirokova <stolar3@yandex.ru>
2025-11-18 12:24:26 +0300

    feat: Генерация ресурса skills

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,23 +7,25 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { configuration } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
-import { getTypeOrmConfig } from './config/typeorm.config';
 import { Test } from './test.entity'
+import { dbConfig, TDbConfig } from './config/typeorm.config';
+import { SkillsModule } from './skills/skills.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration, jwtConfig],
+      load: [configuration, jwtConfig, dbConfig],
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: getTypeOrmConfig,
-      inject: [ConfigService],
+      useFactory: (dbConfig: TDbConfig) => dbConfig,
+      inject: [dbConfig.KEY],
     }),
     TypeOrmModule.forFeature([Test]),
     UsersModule,
     AuthModule,
+    SkillsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
