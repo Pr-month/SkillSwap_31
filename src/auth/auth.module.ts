1eaf794cea19c3b2d502880df5e4605bc732bbff
annpikova <ann.pikova@yandex.ru>
2025-11-29 16:30:34 +0100

    в сервисе auth переписана функция register

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { jwtConfig } from '../config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [ConfigModule.forFeature(jwtConfig)],
+  imports: [
+    ConfigModule.forFeature(jwtConfig),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
