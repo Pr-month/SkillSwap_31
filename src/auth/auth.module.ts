cb950949caf98b24b08bf893ac483b2038ee18b4
stan99 <stan99-cheb@yandex.ru>
2025-12-05 19:59:29 +0300

    fix: исправление auth.module для работы со стратегиями jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
 import { jwtConfig } from '../config/jwt.config';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [ConfigModule.forFeature(jwtConfig), UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
 })
 export class AuthModule {}
