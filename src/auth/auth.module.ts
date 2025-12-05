ba2c24838afdb0685e0459c61eee74c5d8767c3f
stan99 <stan99-cheb@yandex.ru>
2025-12-05 22:59:27 +0300

    fix: замена userService на userRepository

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,14 @@ import { AuthService } from './auth.service';
 import { ConfigModule } from '@nestjs/config';
 import { jwtConfig } from '../config/jwt.config';
 import { Module } from '@nestjs/common';
-import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [ConfigModule.forFeature(jwtConfig), UsersModule],
+  imports: [
+    ConfigModule.forFeature(jwtConfig),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
