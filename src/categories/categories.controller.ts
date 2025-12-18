6450b2eb1f95829440cf8d04c6f5f23cc69f0c7c
Oleg <ol3g-egorov-96@yandex.ru>
2025-12-19 00:30:33 +0300

    chore: accept incoming changes

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -6,43 +6,45 @@ import {
   Patch,
   Param,
   Delete,
-  UseGuards,
-  Req,
-  ForbiddenException,
-  HttpCode
+  // UseGuards,
+  // Req,
+  // ForbiddenException,
 } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { Category } from './entities/category.entity';
-import { Role } from 'src/users/enum';
-import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
-import { ApiTags } from '@nestjs/swagger';
+// import { Role } from 'src/users/enum';
+// import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 
-type AuthRequest = Request & {
-  user: {
-    id: string;
-    role: Role;
-  };
-};
+// type AuthRequest = Request & {
+//   user: {
+//     id: string;
+//     role: Role;
+//   };
+// };
 
-@ApiTags('categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  // @Post()
+  // @UseGuards(JwtAccessGuard)
+  // create(
+  //   @Body() createCategoryDto: CreateCategoryDto,
+  //   @Req() req: AuthRequest,
+  // ): Promise<Category> {
+  //   const user = req.user;
+
+  //   if (user.role !== Role.Admin) {
+  //     throw new ForbiddenException('Недостаточно прав');
+  //   }
+
+  //   return this.categoriesService.create(createCategoryDto);
+  // }
+
   @Post()
-  @UseGuards(JwtAccessGuard)
-  create(
-    @Body() createCategoryDto: CreateCategoryDto,
-    @Req() req: AuthRequest,
-  ): Promise<Category> {
-    const user = req.user;
-
-    if (user.role !== Role.Admin) {
-      throw new ForbiddenException('Недостаточно прав');
-    }
-
+  create(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
     return this.categoriesService.create(createCategoryDto);
   }
 
@@ -57,33 +59,15 @@ export class CategoriesController {
   }
 
   @Patch(':id')
-  @UseGuards(JwtAccessGuard)
   update(
     @Param('id') id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
-    @Req() req: AuthRequest,
-  ): Promise<Category> {
-    const user = req.user;
-
-    if (user.role !== Role.Admin) {
-      throw new ForbiddenException('Недостаточно прав');
-    }
-
+  ) {
     return this.categoriesService.update(id, updateCategoryDto);
   }
 
   @Delete(':id')
-  @UseGuards(JwtAccessGuard)
-  @HttpCode(204)
-  async remove(
-    @Param('id') id: string,
-    @Req() req: AuthRequest,
-  ): Promise<void> {
-    const user = req.user;
-
-    if (user.role !== Role.Admin) {
-      throw new ForbiddenException('Недостаточно прав');
-    }
+  remove(@Param('id') id: string) {
     return this.categoriesService.remove(id);
   }
 }
