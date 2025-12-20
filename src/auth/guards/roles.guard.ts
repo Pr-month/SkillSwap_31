505f65f7458836aa4de952d7c67638402e7ad333
Evgeny <lukin@usota.ru>
2025-12-20 18:48:07 +0300

    feat: добавлены RolesGuard и @Roles для защиты роутов по ролям

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '../../users/enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler());
+    
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user) {
+      throw new ForbiddenException('Недостаточно прав');
+    }
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Недостаточно прав');
+    }
+
+    return true;
+  }
+}
+
