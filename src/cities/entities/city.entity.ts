dd3af77b5248c2f4ce93b95afb5b9823e8310d83
Oleg <ol3g-egorov-96@yandex.ru>
2025-12-26 17:46:02 +0300

    feat: add cities entity

--- src/cities/entities/city.entity.ts
+++ src/cities/entities/city.entity.ts
@@ -1,1 +1,13 @@
-export class City {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('cities')
+export class City {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 100 })
+  subject: string;
+}
